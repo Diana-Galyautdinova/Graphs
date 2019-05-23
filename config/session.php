844fc7c963690d78cd9ef25efe5ace3f193ac752
Diana <Diana.Galyautdinova@tatar.ru>
2019-05-23 15:16:16 +0300

    create repository

---
+++ config/session.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'components' => [
+        'session' => [
+            'class' => 'yii\redis\Session',
+            'redis' => [
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ]
+        ],
+    ],
+
+];
